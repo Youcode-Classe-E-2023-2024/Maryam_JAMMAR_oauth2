5802fbd74ed6ad019eda2c4b5aab6bb29594a152
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-03-27 11:35:28 +0000

    update register

--- OAuth2/app/Http/Controllers/Api/AuthController.php
+++ OAuth2/app/Http/Controllers/Api/AuthController.php
@@ -1,8 +1,10 @@
 <?php
 
+//namespace app\Http\Controllers\Api;
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,8 +13,24 @@ class AuthController extends Controller
         echo "Login Endpoint Requested";
     }
 
-    public function register(){
-        echo "Signup Endpoint Requested";
+    public function register(Request $request){
+//        $request->validate([
+//           'name' => 'required|string',
+//            'email' => 'required|string|unique:users,email,',
+//            'password' => 'required|string|min:8',
+//            'role' => 'required|exists:roles,id',
+//        ]);
+
+        $user =new User([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+            'role' => $request->role
+        ]);
+        $user->save();
+        return response()->json([
+           "message" => "User registred successfully"
+        ], 201);
     }
 
     public function index(){
